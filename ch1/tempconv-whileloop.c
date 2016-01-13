f4b0be1c5a8066d78deb1293df685bd8d6a6947c
Jacob Burden <jacob.burden@qscend.com>
2016-01-13 14:43:08 -0500

    temp converter in a while loop

---
+++ ch1/tempconv-whileloop.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main () {
+  float fahr, celsius;
+
+  int lower = 0;
+  int upper = 300;
+  int step = 20;
+
+  fahr = lower;
+
+  while (fahr <= upper) {
+    celsius = (5.0/9.0) * (fahr-32.0);
+    printf("%3.0f %6.1f\n", fahr, celsius);
+    fahr = fahr + step;
+  }
+}
